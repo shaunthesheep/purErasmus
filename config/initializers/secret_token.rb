8680d264bde90b1fe02800eff179d69bdffa5d89
Quentin Calvez <quentin.calvez@quentez.com>
2012-10-01 16:40:35 +0200

    Created the App and configured the database for postgresql.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purerasmus::Application.config.secret_token = '084888cc23de4dae7d33bdf301be4dbcf5eaa3a41dc23980ca027026ec1805643408f3c8756d929ebea2149f5acdd0acddd14371534d886de9f6b0a8ff4de229'
