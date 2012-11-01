e13f6d36c6210234eb7d6c492f36cd36be96e383
Pierre Laurac <pierre.laurac@gmail.com>
2012-11-01 17:38:17 +0100

    adding relations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+require "bcrypt"
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password
+	# Attributes
+	#-----------
+	attr_accessible :email, :first_name, :last_name, :password
+
+	# Associations
+	#-------------
+	has_many :comments
+	has_many :topics
+	has_one :admin_university, :class_name => "University"
+	belongs_to :home_university, :class_name => "University"
+	belongs_to :foreign_university, :class_name => "University"
+	belongs_to :user_type, :class_name => "UserType"
+	
+	# Validations
+	#------------
+	validates :email, :presence => true, :format => { :with => /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/ }
+	validates :first_name, :presence => true
+	validates :last_name, :presence => true	
 end
