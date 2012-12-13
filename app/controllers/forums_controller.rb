025dd540f8939ac7ca5f673d99edce79cf340037
anna <anna@shaun.(none)>
2012-12-13 16:01:17 +0100

    Working forum with breadCrisps after add

---
+++ app/controllers/forums_controller.rb
@@ -0,0 +1,13 @@
+class ForumsController < ApplicationController
+
+	#shows all countries 
+	def index
+		@countries = Country.all
+	end
+
+	def show
+		@country = Country.find(params[:id])
+		@user = retrieve_authenticated_user
+	end
+
+end
